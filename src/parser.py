e65f2ba9b8731a8fa8b64443f788a4eb2fa33d75
Ashish G Bhargav <ashishgbhargav@gmail.com>
2026-02-20 04:50:41 +0530

    Delete src/parser.py

--- src/parser.py
+++
@@ -1,9 +0,0 @@
-# src/parser.py
-
-def parse_json_data(raw_data):
-    if not raw_data:
-        return None
-    
-    parsed = {"data": raw_data}
-# <--- INDENTATION ERROR: The next line breaks the block scope
-return parsed
